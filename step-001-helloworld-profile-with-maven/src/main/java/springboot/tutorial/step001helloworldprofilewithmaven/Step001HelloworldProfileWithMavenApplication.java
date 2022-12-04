8f6df2be4022ba234af21830db4c68b159e6c92a
samira <Samir.Radmaneshfar@gmail.com>
2022-12-04 16:09:52 +0100

    init commit

---
+++ step-001-helloworld-profile-with-maven/src/main/java/springboot/tutorial/step001helloworldprofilewithmaven/Step001HelloworldProfileWithMavenApplication.java
@@ -0,0 +1,13 @@
+package springboot.tutorial.step001helloworldprofilewithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step001HelloworldProfileWithMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step001HelloworldProfileWithMavenApplication.class, args);
+	}
+
+}
---
+++ step-001-helloworld-profile-with-maven/src/test/java/springboot/tutorial/step001helloworldprofilewithmaven/Step001HelloworldProfileWithMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.tutorial.step001helloworldprofilewithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step001HelloworldProfileWithMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
